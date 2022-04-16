becec7e0518e23cd7f7566700ed9308061078387
Jisung Kim <jisungkim215@gmail.com>
2022-04-15 23:04:46 -0400

    Property and Attribute Binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -10,10 +10,19 @@ import { CoursesService } from './courses.service';
                 {{ course }}
             </li>
         </ul>    
+        <!-- <img src="{{ imageUrl }}" /> -->
+        <img [src]="imageUrl" />
+        <table>
+            <tr>
+                <td [attr.colspan]="colSpan"></td>
+            </tr>
+        </table>
     `
 })
 export class CoursesComponent {
     title = "List of courses";
+    imageUrl = "https://dummyimage.com/400x200/000/fff";
+    colSpan = 2;
     courses;
     
     constructor(service: CoursesService) {
